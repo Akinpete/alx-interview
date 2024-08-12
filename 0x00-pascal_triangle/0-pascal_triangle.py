5258ca1aa349cf8bbe5d5fa7ff9c353ae345393c
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-08-12 05:24:58 +0100

    PEP8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,21 +4,21 @@
 """
 
 
-def pascal_triangle(n):
+def pascal_triangle(n: int) -> list:
     """
     Returns a list of integers
     representing the Pascal Triangle of n
     returns empty list if n <= 0
     """
-    k = []
+    triangle = []
     if n <= 0:
-        return k
-    k = [[1]]
+        return triangle
+    triangle = [[1]]
     for i in range(1, n):
         temp = [1]
-        for j in range(len(k[i - 1]) - 1):
-            curr = k[i - 1]
-            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        for j in range(len(triangle[i - 1]) - 1):
+            curr = triangle[i - 1]
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         temp.append(1)
-        k.append(temp)
-    return k
+        triangle.append(temp)
+    return triangle
