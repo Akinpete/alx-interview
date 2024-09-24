205915689176043028fc76ac645ac5678111db19
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-24 09:21:12 +0100

    sighs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,44 +2,16 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  const fetchCharacter = (index) => {
-    if (index >= characters.length) return;
-
-    request(characters[index], (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character:', charError);
-        fetchCharacter(index + 1);
-        return;
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Invalid character status code:', charResponse.statusCode);
-        fetchCharacter(index + 1);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-      fetchCharacter(index + 1);
-    });
-  };
-
-  fetchCharacter(0);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
